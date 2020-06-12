b3ca8fd6696757892892c36027a439a9050b21d4
youwugang <youwugang@onething.net>
2020-06-12 14:48:54 +0800

    init.

---
+++ index.js
@@ -0,0 +1,38 @@
+const { createFilter } = require('@rollup/pluginutils');
+const MagicString = require('magic-string');
+
+const newStr = 'I am rollup.js!';
+
+module.exports = function replaceBlocks(options = {}) {
+  const filter = createFilter(options.include, options.exclude);
+  const sourcemap = options.sourcemap === true;
+
+  return {
+    name: 'helloworld',
+
+    transform(code, id) {
+      if (!filter(id)) return;
+
+      let codeStr = `${code}`;
+      const magic = new MagicString(codeStr);
+      codeStr = codeStr.replace(/\/\/replace-block:begin[\w\W]*\/\/replace-block:end/gi, function (match, offset) {
+        const start = offset;
+        const end = offset + match.length;
+        magic.overwrite(start, end, '');
+        return '';
+      });
+
+      const resultCode = magic.toString();
+      let resultMap = false;
+      if (sourcemap === true) {
+        resultMap = magic.generateMap({
+          hires: true,
+        });
+      }
+      return {
+        code: resultCode,
+        map: resultMap,
+      };
+    },
+  };
+}
